db5624a7cc7987bcb146ef5c2b0fac577fa1b9a6
AnnaMarie H <annamarie.hurst@vml.com>
2015-08-27 15:44:59 -0500

    repair more dimensional issues. set everything to percentages, adopt mobile view for almost all sectors (block on services). Implement bootstrap glyphicon correctly to further develop mobile view.

--- js/main.js
+++ js/main.js
@@ -4,5 +4,8 @@ $(function(){
         var sector= $(this).attr("href");
         $("#home, #services, #contact, #story").css("z-index", -1);
         $(sector).css("z-index", 0);
-    })
-})
+    });
+    $(".hamburger-menu").on("click", function(){
+        $(".main-header ul").slideToggle();
+    });
+});
